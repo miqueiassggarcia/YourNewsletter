a9947ba51846de32e7f9ee4473629c10945bf10e
Miquéias Garcia <mglucenalucena@gmail.com>
2023-09-06 10:52:31 -0300

    feature/NEW-37-modificando design da tela

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,28 @@
-import './App.css';
+import './styles/App.css';
 import { Input } from './components/Input';
 import { ConfirmButton } from './components/ConfirmButton';
 
 function App() {
   return (
     <div className="App">
+      <div className="float-div">
+        <div className='container-text'>
+          <h1>Bem vindo de volta!</h1>
+          <span>Faça login aqui e continue a sua newsletter</span>
+          <button type='button'>Login</button>
+        </div>
         <form className='login-form'>
-          <h2>Cadastre-se</h2>
-          <Input type="text" name="nome" id="nome" placeholder='Digite um nome'/>
-          <Input type="email" name="email" id="email" placeholder='Digite seu email'/>
-          <Input type="text" name="name" id="senha" placeholder='Digite uma senha'/>
-          <span>Já tem uma conta: <a href="">login</a></span>
+          <h1>Cadastre-se</h1>
+          <div className='input-container'>
+            <Input type="text" name="nome" id="nome" placeholder='Digite um nome'/>
+            <Input type="email" name="email" id="email" placeholder='Digite seu email'/>
+            <Input type="text" name="name" id="senha" placeholder='Digite uma senha'/>
+          </div>
           <ConfirmButton>Cadastre-se</ConfirmButton>
         </form>
+      </div>
     </div>
+    
   );
 }
 
