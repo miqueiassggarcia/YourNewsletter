07e082bf6223232fa304909acd8c9bacec85e9de
roxy22788 <roxy2298gm@gmail.com>
2023-09-07 11:19:41 -0300

    criação da rota de confirmação de email e melhorias gerais

--- Backend/src/test.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const randToken = require("rand-token");
-
-console.log(process.env.CONFIRMATION_TOKEN_DICTIONARY);
-console.log(randToken.generate(6, process.env.CONFIRMATION_TOKEN_DICTIONARY));
