1e8b22f759182bc45340bc2bd923bb16b1b0cea4
Miquéias Garcia <59172718+miqueiassggarcia@users.noreply.github.com>
2023-09-09 16:15:21 -0300

    Feature/new 37 (#5)
    * Criando estilo da tela de cadastro
    
    * feature/NEW-37-modificando design da tela
    
    * Organizando estrutura dos arquivos
    
    * Criando responsividade para mobile

--- Frontend/src/tests/App.test.tsx
+++ Frontend/src/tests/App.test.tsx
@@ -1,15 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-test("text of react example", () => {
-  const { getByText } = render(<App />)
-
-  expect(getByText('Learn React')).toBeTruthy()
 });
