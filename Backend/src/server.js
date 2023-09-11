9ce775f453cac4827ff00c8d6dc738a42042a6d0
Miquéias Garcia <mglucenalucena@gmail.com>
2023-09-11 11:20:55 -0300

    Usando o cors no backend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,7 @@ const sendEmail = require("./email_service");
 
 require('dotenv').config()
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const bodyParser = require('body-parser');
 const randToken = require("rand-token");
@@ -15,6 +16,7 @@ const prisma = new PrismaClient();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors())
 
 
 
