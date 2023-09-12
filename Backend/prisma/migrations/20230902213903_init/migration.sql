3dc170edec1b249d5d7cb2ef60d517cbada0abba
Miquéias Garcia <59172718+miqueiassggarcia@users.noreply.github.com>
2023-09-12 11:46:30 -0300

    Revert "Develop (#13)" (#14)
    This reverts commit 9f4df57e234e253a71c9fd026ec39ce94b1a21b2.

--- Backend/prisma/migrations/20230902213903_init/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "EmailConfirmation" ALTER COLUMN "token_generate_time" SET DATA TYPE TEXT,
-ALTER COLUMN "token_expiry_time" SET DATA TYPE TEXT,
-ALTER COLUMN "token_confirmation" SET DATA TYPE TEXT;
