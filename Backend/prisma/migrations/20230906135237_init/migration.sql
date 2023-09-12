024b05ef64f1172894c0d2c1614fbdf81ee6d57e
Miquéias Garcia <59172718+miqueiassggarcia@users.noreply.github.com>
2023-09-12 11:45:56 -0300

    Revert "Develop (#13)"
    This reverts commit 9f4df57e234e253a71c9fd026ec39ce94b1a21b2.

--- Backend/prisma/migrations/20230906135237_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `password` on the `EmailConfirmation` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
-
-*/
--- AlterTable
-ALTER TABLE "EmailConfirmation" ALTER COLUMN "password" SET DATA TYPE VARCHAR(255);
