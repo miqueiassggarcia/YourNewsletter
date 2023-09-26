52abc0cab128e613aa6ac4f08fc1a3a90f42b810
roxy22788 <roxy2298gm@gmail.com>
2023-09-26 09:39:22 -0300

    remoção de lixo

--- Backend/src/test.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-let senha = "euqueromorrer";
-let senha2 = "euqueromorrerr";
-let hash = bcrypt.hashSync(senha, parseInt('12'));
-let hash2 = bcrypt.hashSync(senha, 13);
-console.log(bcrypt.compareSync(senha, hash));
-console.log(bcrypt.compareSync(senha2, hash2));
