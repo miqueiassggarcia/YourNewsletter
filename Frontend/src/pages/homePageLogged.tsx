f9f3c6fae3ebcd033f941dc0c9d0059265192e5e
Miquéias Garcia <mglucenalucena@gmail.com>
2023-10-02 15:10:01 -0300

    Remodelando frontend para as funcionalidades do back

--- Frontend/src/pages/homePageLogged.tsx
+++ Frontend/src/pages/homePageLogged.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function HomePageLogged() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const validate = localStorage.getItem("validate");
+    if(!validate) {
+      navigate("/")
+    }
+  }, [navigate])
+
   return (
     <></>
   );
