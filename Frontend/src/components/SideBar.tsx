f99c48eddcdb9799e66dc7e6283d42cbe76f4d67
Miquéias Garcia <mglucenalucena@gmail.com>
2023-10-03 11:50:42 -0300

    Adicionando funcionalidade de logout e adicionando layout da home

--- Frontend/src/components/SideBar.tsx
+++ Frontend/src/components/SideBar.tsx
@@ -1,18 +1,42 @@
 import { AiFillCloseCircle, AiFillHome, AiFillSetting } from "react-icons/ai"
-
-import "../styles/sidebar.css"
+import { BiLogOut } from "react-icons/bi"
 import { IoNewspaperSharp } from "react-icons/io5"
 import { FaUserCircle } from "react-icons/fa"
+
+import "../styles/sidebar.css"
+import api from "../services/api"
 
 type activeProps = {
   active: () => void
 }
 
 export function SideBar({ active }: activeProps) {
+  async function handleLogout() {
+    await api.post("/logout")
+    .then(() => {
+      localStorage.removeItem("username")
+      localStorage.removeItem("first_name")
+      localStorage.removeItem("last_name")
+      localStorage.removeItem("email")
+      localStorage.removeItem("validate")
+    })
+    .catch((error) => {
+      if(error.response.status === 401) {
+        alert("Erro ao fazer logout, por favor, tente novamente")
+      }
+    })
+  }
+
   return (
     <div className="sidebar-container">
-      <AiFillCloseCircle onClick={active} className="close-button"/>
       <div className="sidebar-items">
+        <div className="sidebar-buttons">
+          <div className="logout" onClick={handleLogout}>
+          <BiLogOut className="logout-button" size={25}/>
+          <span>logout</span>
+          </div>
+          <AiFillCloseCircle onClick={active} className="close-button" size={25}/>
+        </div>
         <div className="items-elements">
           <span>Home</span>
           <AiFillHome className="sidebar-items-icons" size={25}/>
--- Frontend/src/pages/homePageLogged.tsx
+++ Frontend/src/pages/homePageLogged.tsx
@@ -1,6 +1,19 @@
+import { ConfirmButton } from "../components/ConfirmButton";
+import "../styles/homePageLogged.css"
+
 export function HomePageLogged() {
   return (
-    <div style={{height: "100%"}}>
+    <div className="home-logged-container">
+      <div className="new-newsletter-container">
+        <h1>Crie uma nova newsletter</h1>
+        <p>Crie a sua própria newsletter, definindo seus textos, horário e muito mais!</p>
+        <ConfirmButton type="button">Criar newsletter</ConfirmButton>
+      </div>
+      <div className="subscribe-newsletter-container">
+        <h1>Se increva na sua newsletter favorita</h1>
+        <p>Procure o que mais lhe agrada e encontre as melhores newsletters para melhorar o seu dia!</p>
+        <ConfirmButton type="button">Procurar agora</ConfirmButton>
+      </div>
     </div>
   );
 }
