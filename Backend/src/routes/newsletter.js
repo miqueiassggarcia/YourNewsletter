5db1e0e22577c52a7abbc23e1bdee4ba32d40eeb
roxy22788 <roxy2298gm@gmail.com>
2023-10-30 17:27:37 -0300

    add rota /my_subscribed_newsletters | add rota /check_newsletter_subscription

--- Backend/src/routes/newsletter.js
+++ Backend/src/routes/newsletter.js
@@ -6,6 +6,7 @@ const update_newsletter_name_schema = require('../validation/update_newsletter_n
 const update_newsletter_description_schema = require('../validation/update_newsletter_description.js');
 const delete_newsletter_schema = require('../validation/delete_newsletter.js');
 const newsletter_subscribe_schema = require('../validation/newsletter_subscribe.js');
+const check_newsletter_subscription_schema = require('../validation/check_newsletter_subscription.js');
 
 module.exports = function (app, prisma, http_status) {
     app.post('/create_newsletter', ensureAuthenticated,
@@ -448,4 +449,54 @@ module.exports = function (app, prisma, http_status) {
             return res.status(code).json({"message": message});
         } 
     });
+
+    
+    app.get('/my_subscribed_newsletters', ensureAuthenticated,
+    async (req, res, next) => {
+        try {
+            let newsletters = await prisma.newsLetter.findMany({
+                where: {
+                    newsletter_subscribers: {
+                        some: {
+                            user_username: req.user.username
+                        }
+                        
+                    }
+                }
+            });
+    
+            return res.status(200).json(newsletters);
+        } catch (error) {
+            console.log(error);
+            const {code, message} = http_status.get_database_error();
+            res.status(code).json({"message": message}); 
+        }
+    });
+
+
+    app.get('/check_newsletter_subscription', ensureAuthenticated,
+    (req, res, next) => {
+        const {error} = check_newsletter_subscription_schema.validate(req.query);
+        if (error) {
+            return res.status(400).json({"message": error.details[0].message});
+        } else {
+            next();
+        }
+    },
+    async (req, res) => {
+        try {
+            let subscription = await prisma.newsletterSubscribers.findFirst({
+                where: {
+                    user_username: req.user.username,
+                    id_newsletter: parseInt(req.query.id_newsletter)
+                }
+            })
+    
+            return res.status(200).json({"message": subscription != null});
+        } catch (error) {
+            console.log(error);
+            const {code, message} = http_status.get_database_error();
+            res.status(code).json({"message": message}); 
+        }
+    })
 }
