7a03ddbd914767b39f87dfdfb886e903efb6c470
Miquéias Garcia <mglucenalucena@gmail.com>
2023-11-24 16:06:25 -0300

    Salvando estado do design

--- Frontend/src/pages/newsletter/createNewsletterPostPage.tsx
+++ Frontend/src/pages/newsletter/createNewsletterPostPage.tsx
@@ -3,26 +3,49 @@ import { useRef } from 'react';
 import EmailEditor, { EditorRef, EmailEditorProps } from 'react-email-editor';
 
 import "../../styles/newsletter/createNewsletterPost.css";
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { useNavigate } from "react-router-dom"
+import { relative } from 'path';
+import { postCacheProps } from './userNewslettersPage';
 
 export default function CreateNewsletterPostPage() {
+  const navigate = useNavigate();
   const emailEditorRef = useRef<EditorRef>(null);
 
   const exportHtml = () => {
     const unlayer = emailEditorRef.current?.editor;
 
     unlayer?.exportHtml((data) => {
-      const { design, html } = data;
+      const { html } = data;
       console.log('exportHtml', html);
     });
   };
 
+  const saveDesign = () => {
+    const unlayer = emailEditorRef.current?.editor;
+
+    unlayer?.exportHtml((data) => {
+      const { design } = data;
+      localStorage.setItem("test", JSON.stringify(design));
+      // let localStorageDesign = localStorage.getItem("Newsletter");
+      // let currentDesign: postCacheProps = JSON.parse(localStorageDesign ? localStorageDesign : "");
+
+      // currentDesign.data = design;
+
+      // localStorage.setItem("Newsletter", JSON.stringify(currentDesign));
+    });
+  };
+
   const onReady: EmailEditorProps['onReady'] = (unlayer) => {
-    // const templateJson = { DESIGN JSON GOES HERE };
-    // unlayer.loadDesign(templateJson);
+    let localStorageDesign = localStorage.getItem("Newsletter")!;
+    // let currentDesign: postCacheProps = JSON.parse(localStorageDesign);
+    let currentDesign = JSON.parse(localStorage.getItem("test")!);
+    unlayer.loadDesign(currentDesign);
   };
 
   return (
     <div className='create-newsletter-post-container'>
+      <AiOutlineArrowLeft size={30} className="create-newsletter-post-back-button" onClick={() => {saveDesign(); navigate(-1);}}/>
       <EmailEditor ref={emailEditorRef} onReady={onReady} />
       <div className='create-newsletter-post-button-container'>
         <button className='create-newsletter-post-button' onClick={exportHtml}>Export HTML</button>
--- Frontend/src/pages/newsletter/userNewslettersPage.tsx
+++ Frontend/src/pages/newsletter/userNewslettersPage.tsx
@@ -4,9 +4,17 @@ import api from "../../services/api";
 
 import "../../styles/newsletter/userNewsletters.css"
 import NewsletterUserItem from "../../components/NewsletterUserItem";
-import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlineArrowLeft, AiOutlinePlus } from "react-icons/ai";
+import { useNavigate } from "react-router";
+import { JSONTemplate } from "state/types/index";
+
+export interface postCacheProps {
+  newsletter_id?: number,
+  data?: JSONTemplate
+}
 
 export function UserNewslettersPage() {
+  const navigate = useNavigate();
   const [newsletters, setNewsletters] = useState<newsletterProps[]>([]);
   const [postsIsOpen, setPostIsOpen] = useState<boolean>(false);
   const [newsletterSelectedId, setNewsletterSelectedId] = useState<number>();
@@ -28,16 +36,26 @@ export function UserNewslettersPage() {
     getUserNewsletters();
   }, [])
 
+  function setStateOfTheCurrentPost() {
+    let postData: postCacheProps = {
+      "newsletter_id": newsletterSelectedId,
+      // "data": 
+    }
+    localStorage.setItem("Newsletter", JSON.stringify(postData));
+    console.log(JSON.parse(localStorage.getItem("Newsletter")!));
+  }
+
   return (
     <div className="container-user-newsletters">
       {postsIsOpen ? 
-      <>
-        <button className="create-newsletter-post-button">
+      <div className="create-newsletter-posts-container">
+        <AiOutlineArrowLeft size={30} className="create-newsletter-posts-back-button" onClick={closePosts}/>
+        <button className="create-newsletter-posts-button" onClick={() => {setStateOfTheCurrentPost(); navigate("/create-newsletter-post")}}>
           Adicionar post
-          <AiOutlinePlus size={25} className="create-newsletter-post-icon"/>
+          <AiOutlinePlus size={25} className="create-newsletter-posts-icon"/>
         </button>
         <p>{newsletterSelectedId}</p>
-      </>
+      </div>
       :
       <>
         {newsletters.map((newsletter) => {
